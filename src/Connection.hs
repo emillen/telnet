b03ac8b681268b28ca0fa8da83ac25f672956085
Emil Lengman <emillen@kth.se>
2016-05-22 22:37:50 +0200

    comments

--- src/Connection.hs
+++ src/Connection.hs
@@ -1,9 +1,11 @@
+-- | The module for handling connections
 module Connection(startConn) where
-
 import Network.Socket
 import System.IO
 
-startConn :: Socket -> IO ()
+-- | Handles a connection to a client, writing everything it sends to the command line
+startConn ::  Socket -- ^ The client socket
+                  -> IO () 
 startConn client = do
   hdl <- socketToHandle client ReadMode
   hSetBuffering hdl NoBuffering
--- src/Main.hs
+++ src/Main.hs
@@ -1,15 +1,22 @@
+-- | Main module
+module Main where
+
+
 import Connection
 import Control.Concurrent
 import Network.Socket
 
-main::IO()
+-- | Main function
+main::IO ()
 main = do
   sock <- socket AF_INET Stream defaultProtocol
   bind sock $ SockAddrInet 3000 iNADDR_ANY
   listen sock 2
   connectionHandler sock
-  
-connectionHandler :: Socket -> IO ()
+
+-- | Handles connections requested on a socket 
+connectionHandler :: Socket -- ^ The socket to accept connections on 
+                                  -> IO () 
 connectionHandler sock = do
   (client, _) <- accept sock
   _ <- forkIO (startConn client)
